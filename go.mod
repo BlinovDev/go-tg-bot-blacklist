ae31f08447c7fcda11cfada659bc0c132c926c7c
Anton <anton253647@gmail.com>
2024-11-08 20:46:28 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/BlinovDev/go-tg-bot-blacklist
+
+go 1.22.4
---
+++ tgblacklist.go
@@ -0,0 +1,8 @@
+package tgblacklist
+
+import "fmt"
+
+// Greet prints a greeting message.
+func Greet(name string) {
+	fmt.Printf("Hello, %s!\n", name)
+}
